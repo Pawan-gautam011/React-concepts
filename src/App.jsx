33aa206254fa53642951740fbc932d7bc75e4950
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-07-08 23:54:02 +0545

    Header Section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Contact from './Contact'
+import Globalstyle from './GlobalStyle'
 import Home from './Home'
 import Services from './Services'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
@@ -25,14 +26,16 @@ function App() {
       gradient:
       "Linear-gradient(0deg, rgb(132,144,255) 0%, rgb(98,189,252) 100%  )",
       shadow:
-      "rgba(0,0,0,0.02) 0px 1px 3px 0px, rgba(27,31,35,0.15) 0px 0px  ",
+      "rgba(0,0,0,0.02) 0px 1px 3px 0px, rgba(27,31,35,0.15) 0px 0px 1px ",
 
       shadowSupport:"rgba(0,0,0,0.16) 0px 1px 4px"
-    }
-  }
+    },
+    media:{mobile:"768px", tab:"998px"},
+   }
 
 return (
   <ThemeProvider theme={theme}>
+    <Globalstyle/>
   <BrowserRouter>
   <Header/>
   <Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Navbar from './Navbar'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
-    <header>
+    <MainHeader>
         <NavLink to="/">
         <h1 style={{}}>Pa<span style={{color:"Blue"}}>W</span>An</h1>
         </NavLink>
         <Navbar/>
-    </header>  
+    </MainHeader>  
 )
 }
+
+const MainHeader = styled.header`
+  padding:0 4.8rem;
+  height:10rem;
+  background-color:${({theme}) => theme.colors.bg};
+  display:flex;
+  justify-content : space-between ;
+  align-items:center;
+
+  h1{
+    height:auto;
+    max-width: 30%;
+  }
+`
 
 export default Header
